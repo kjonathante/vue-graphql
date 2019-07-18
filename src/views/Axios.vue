6e39bb659d441d0fad82096c9d3739added12dba
Kit Jonathan Te <kjonathante@gmail.com>
2019-07-17 23:08:43 -0700

    Used axios to make a graphql call to an api

--- src/views/Axios.vue
+++ src/views/Axios.vue
@@ -1,5 +1,50 @@
 <template>
-  <div class="axios">
-    <h1>This is an axios page</h1>
+  <div>
+    <button @click="getLanguage">Get Data</button>
+    <ul>
+      <li v-for="result in results">{{ result.id }}</li>
+    </ul>
   </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'app',
+  data() {
+    return {
+      results: []
+    }
+  },
+  methods: {
+    async getLanguage() {
+      try {
+        const res = await axios.post('https://api.kivaws.org/graphql', {
+          query: `{
+            general {
+              partners {
+                values {
+                  id,
+                  name,
+                  countries {
+                    name
+                    isoCode
+                    region
+                    ppp
+                    numLoansFundraising
+                    fundsLentInCountry
+                  }
+                }
+              }
+            }
+          }`
+        })
+        // this.example1 = res.data.data.language
+        console.log(res.data.data.general.partners.values)
+        this.results = res.data.data.general.partners.values
+      } catch (e) {
+        console.log('err', e)
+      }
+    }
+  }
+}
+</script>
