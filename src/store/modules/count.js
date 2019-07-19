f385fd9f6f98aa6460a218e032d1596d65823fbb
Kit Jonathan Te <kjonathante@gmail.com>
2019-07-19 14:13:32 -0700

    Added the todos module. Modified the store to include the todos module. Modified the Vuex component to work with the modified store.

--- src/store/modules/count.js
+++ src/store/modules/count.js
@@ -1,14 +1,36 @@
 // state
-const state = {}
+const state = {
+  count: 0
+}
 
 // getters
 const getters = {}
 
 // actions
-const actions = {}
+const actions = {
+  increment(context) {
+    // context.commit
+    // context.state
+    // comtext.getters
+    // context.dispatch
+    context.commit('increment')
+  },
+  decrement({ commit }) {
+    commit('decrement')
+  },
+  incrementAsync({ commit }, payload) {
+    setTimeout(() => {
+      commit('increment')
+      console.log(payload)
+    }, 1000)
+  }
+}
 
 // mutations
-const mutations = {}
+const mutations = {
+  increment: state => state.count++,
+  decrement: state => state.count--
+}
 
 export default {
   namespaced: true,
